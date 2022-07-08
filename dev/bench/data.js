3ba57fec3caecf44a85fa30f44d704ebf7df6b81
github-action-benchmark <github@users.noreply.github.com>
2022-07-08 20:30:33 +0000

    add Orchard Benchmarks (cargo) benchmark result for f81da7a0a476d0c87908f234f954e0687ef25afb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1657312090260,
+  "lastUpdate": 1657312232993,
   "repoUrl": "https://github.com/mschoenebeck/zeos-orchard",
   "entries": {
     "Orchard Benchmarks": [
@@ -47,6 +47,54 @@ window.BENCHMARK_DATA = {
             "name": "proving/bundle/4",
             "value": 9801202501,
             "range": "± 170717803",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias.schoenebeck@gmail.com",
+            "name": "mschoenebeck",
+            "username": "mschoenebeck"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f81da7a0a476d0c87908f234f954e0687ef25afb",
+          "message": "Update README.md",
+          "timestamp": "2022-07-09T04:17:47+08:00",
+          "tree_id": "b1b5de53feebe52990b40bea44ebb3f603454927",
+          "url": "https://github.com/mschoenebeck/zeos-orchard/commit/f81da7a0a476d0c87908f234f954e0687ef25afb"
+        },
+        "date": 1657312231212,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "proving/bundle/1",
+            "value": 5219654847,
+            "range": "± 67443893",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/2",
+            "value": 5181968823,
+            "range": "± 49212861",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/3",
+            "value": 7505974840,
+            "range": "± 54720378",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/4",
+            "value": 9744818146,
+            "range": "± 99651598",
             "unit": "ns/iter"
           }
         ]
