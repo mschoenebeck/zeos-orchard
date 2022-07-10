4b190187c51e23aa5f6c973d673b7c29f3301e12
github-action-benchmark <github@users.noreply.github.com>
2022-07-10 08:05:28 +0000

    add Orchard Benchmarks (cargo) benchmark result for 54787e0bb07459cd146262dc20991d732834a947

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1657440328241,
+  "repoUrl": "https://github.com/mschoenebeck/zeos-orchard",
+  "entries": {
+    "Orchard Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "matthias.schoenebeck@gmail.com",
+            "name": "mschoenebeck",
+            "username": "mschoenebeck"
+          },
+          "committer": {
+            "email": "matthias.schoenebeck@gmail.com",
+            "name": "mschoenebeck",
+            "username": "mschoenebeck"
+          },
+          "distinct": true,
+          "id": "54787e0bb07459cd146262dc20991d732834a947",
+          "message": "removed 'user' docs from book. finished note-commit.md",
+          "timestamp": "2022-07-10T02:54:28-05:00",
+          "tree_id": "0e37555d075bbde0c3a0bee78a0c68278f148307",
+          "url": "https://github.com/mschoenebeck/zeos-orchard/commit/54787e0bb07459cd146262dc20991d732834a947"
+        },
+        "date": 1657440327065,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "proving/bundle/1",
+            "value": 4384893434,
+            "range": "± 196757704",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/2",
+            "value": 4385309498,
+            "range": "± 10985385",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/3",
+            "value": 6287043286,
+            "range": "± 17039665",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "proving/bundle/4",
+            "value": 8187651056,
+            "range": "± 14215293",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
